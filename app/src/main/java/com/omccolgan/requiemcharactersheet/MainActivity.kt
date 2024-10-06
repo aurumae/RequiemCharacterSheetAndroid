7972651da3a71df68a707c1ffa7bdcc7a93d3c96
Oisin McColgan <oisindmccolgan@gmail.com>
2024-10-06 14:23:39 +0100

    Try to fix interactivity for Touchstones

--- app/src/main/java/com/omccolgan/requiemcharactersheet/MainActivity.kt
+++ app/src/main/java/com/omccolgan/requiemcharactersheet/MainActivity.kt
@@ -5,14 +5,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.omccolgan.requiemcharactersheet.ui.theme.RequiemCharacterSheetTheme
 
@@ -63,6 +67,19 @@ class MainActivity : ComponentActivity() {
                         }
 
                         item {
+                            // Attributes header
+                            Text(
+                                text = "ATTRIBUTES",
+                                fontFamily = CaslonAntique,
+                                fontSize = 24.sp,
+                                color = Color(0xFF85858A),
+                                lineHeight = 50.sp,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(vertical = 8.dp)
+                                    .background(color = Color(0xFFF2F2F7))
+                            )
+
                             // Attributes List
                             AttributesList(
                                 attributes = character.attributes,
--- app/src/main/java/com/omccolgan/requiemcharactersheet/TouchstoneList.kt
+++ app/src/main/java/com/omccolgan/requiemcharactersheet/TouchstoneList.kt
@@ -4,37 +4,59 @@ package com.omccolgan.requiemcharactersheet
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.foundation.gestures.detectTapGestures
 
 @Composable
 fun TouchstoneList(
     touchstones: List<Touchstone>,
-    humanity: Int, // Add humanity parameter
-    onHumanityChange: (Int) -> Unit, // Callback for humanity change
+    humanity: Int,
+    onHumanityChange: (Int) -> Unit,
     onTextChange: (String, String) -> Unit
 ) {
-    Column {
-        touchstones.sortedByDescending { it.level }.forEach { touchstone ->
-            TouchstoneRow(
-                touchstone = touchstone,
-                humanity = humanity,
-                onHumanityChange = onHumanityChange,
-                onTextChange = onTextChange
+    val focusManager = LocalFocusManager.current
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    focusManager.clearFocus()
+                })
+            }
+    ) {
+        Column {
+            // Header Text
+            Text(
+                text = "HUMANITY",
+                fontFamily = CaslonAntique,
+                fontSize = 24.sp,
+                color = VampireRed,
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
             )
+
+            touchstones.sortedByDescending { it.level }.forEach { touchstone ->
+                TouchstoneRow(
+                    touchstone = touchstone,
+                    humanity = humanity,
+                    onHumanityChange = onHumanityChange,
+                    onTextChange = onTextChange
+                )
+            }
         }
     }
 }
@@ -48,6 +70,8 @@ fun TouchstoneRow(
 ) {
     var textState by remember { mutableStateOf(touchstone.text) }
     var isEditing by remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
 
     Row(
         modifier = Modifier
@@ -60,51 +84,69 @@ fun TouchstoneRow(
             fontFamily = CaslonAntique,
             fontSize = 22.sp,
             color = VampireRed,
-            modifier = Modifier.width(30.dp)
+            modifier = Modifier
+                .width(30.dp)
+                .clickable{ isEditing = false }
         )
 
         // Touchstone Text or TextField
-        if (isEditing) {
-            TextField(
-                value = textState,
-                onValueChange = { newText ->
-                    textState = newText
-                    onTextChange(touchstone.id, newText)
-                },
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(horizontal = 8.dp),
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.Transparent,
-                    unfocusedContainerColor = Color.Transparent,
-                    disabledContainerColor = Color.Transparent,
-                    errorContainerColor = Color.Transparent,
-                    focusedTextColor = Color.White,
-                    unfocusedTextColor = Color.White,
-                    disabledTextColor = Color.White,
-                    focusedPlaceholderColor = Color.Gray,
-                    unfocusedPlaceholderColor = Color.Gray,
-                    disabledPlaceholderColor = Color.Gray,
-                    errorPlaceholderColor = Color.Gray
-                ),
-                placeholder = {
-                    Text("Add Touchstone", color = Color.Gray.copy(alpha = 0.5f))
-                },
-                singleLine = true
-            )
-        } else {
-            val textColor = Color.Gray
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 8.dp)
+        ) {
+            if (isEditing) {
+                TextField(
+                    value = textState,
+                    onValueChange = { newText ->
+                        textState = newText
+                        onTextChange(touchstone.id, newText)
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .focusRequester(focusRequester),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent,
+                        disabledContainerColor = Color.Transparent,
+                        errorContainerColor = Color.Transparent,
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Color.Black,
+                        disabledTextColor = Color.Black,
+                        focusedPlaceholderColor = Color.Gray,
+                        unfocusedPlaceholderColor = Color.Gray,
+                        disabledPlaceholderColor = Color.Gray,
+                        errorPlaceholderColor = Color.Gray
+                    ),
+                    placeholder = {
+                        Text("Add Touchstone", color = Color.Gray.copy(alpha = 0.5f))
+                    },
+                    singleLine = true
+                )
 
-            Text(
-                text = if (textState.isEmpty()) "Add Touchstone" else textState,
-                color = textColor.copy(alpha = if (textState.isEmpty()) 0.5f else 1f),
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(horizontal = 8.dp)
-                    .clickable { isEditing = true },
-                fontFamily = CaslonAntique,
-                fontSize = 18.sp
-            )
+                LaunchedEffect(Unit) {
+                    focusRequester.requestFocus()
+                }
+            } else {
+                val textColor = Color.Gray
+
+                Text(
+                    text = if (textState.isEmpty()) "Add Touchstone" else textState,
+                    color = textColor.copy(alpha = if (textState.isEmpty()) 0.5f else 1f),
+                    textDecoration = if ((touchstone.level > humanity) && !textState.isEmpty()) {
+                        TextDecoration.LineThrough
+                    } else null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            isEditing = false
+                            focusManager.clearFocus()
+                            isEditing = true
+                                   },
+                    fontFamily = CaslonAntique,
+                    fontSize = 18.sp
+                )
+            }
         }
 
         // Dot Rating
@@ -112,19 +154,21 @@ fun TouchstoneRow(
             modifier = Modifier
                 .size(20.dp)
                 .padding(end = 8.dp)
-            .clickable {
-                onHumanityChange(touchstone.level)
-            }
+                .clickable {
+                    onHumanityChange(touchstone.level)
+                    //focusManager.clearFocus()
+                    isEditing = false
+                }
         ) {
-        drawCircle(
-            color = if (touchstone.level <= humanity) VampireRed else Color.Transparent,
-            radius = size.minDimension / 2
-        )
-        drawCircle(
-            color = Color.Black,
-            radius = size.minDimension / 2,
-            style = Stroke(width = 1.dp.toPx())
-        )
-    }
+            drawCircle(
+                color = if (touchstone.level <= humanity) VampireRed else Color.Transparent,
+                radius = size.minDimension / 2
+            )
+            drawCircle(
+                color = Color.Black,
+                radius = size.minDimension / 2,
+                style = Stroke(width = 1.dp.toPx())
+            )
+        }
     }
 }
